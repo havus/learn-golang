febd0d8eb47fbe1f981a6399e4ad6eb7631a7e1d
Hafidz Mahrus <havus.it@gmail.com>
2021-06-03 21:38:13 +0800

    move into 1 file

---
+++ package.go
@@ -0,0 +1,32 @@
+package main
+
+import (
+	// "os"
+	// "flag"
+	"fmt"
+)
+
+func main() {
+	// >>>>>>>>>>> PACKAGE OS
+	// $ go run package.go coba argumen
+	// args := os.Args
+	// fmt.Println(args)
+
+	// fmt.Println(args[1])
+	// fmt.Println(args[2])
+
+	// hostname, _ := os.Hostname()
+	// fmt.Println(hostname)
+
+	// >>>>>>>>>>> PACKAGE Flag
+	// $ go run package.go -host="hello world" -key=root
+	// var host *string = flag.String("host", "defaultHost", "Just a description")
+	// *key = flag.String("key", "defaultKey", "Insert ur key")
+	// // var key *string = flag.String("key", "defaultKey", "Insert ur key")
+	// // host := flag.String("hostname", "defaultValue", "Just a description")
+
+	// flag.Parse()
+
+	// fmt.Println(*host)
+	// fmt.Println(*key)
+}
